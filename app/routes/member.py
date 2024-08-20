b65fb4ff6baf54d3d2270d892e434f4e790a96ac
pq100 <system32bit@naver.com>
2024-08-20 14:15:17 +0900

    클라이언트가 전송한 회원정보를 member 테이블에 저장하고 결과를 클라이언트에게 반환하는 /join 엔드포인트 작성

--- app/routes/member.py
+++ app/routes/member.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.orm import Session
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
+
+from app.dbfactory import get_db
+from app.schema.member import NewMember
+from app.service.member import MemberService
 
 member_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
@@ -8,6 +13,13 @@ templates = Jinja2Templates(directory='views/templates')
 @member_router.get('/join', response_class=HTMLResponse)
 async def join(req: Request):
     return templates.TemplateResponse('member/join.html', {'request' : req})
+
+@member_router.post('/join', response_class=HTMLResponse)
+async def joinok(member: NewMember, db: Session = Depends(get_db)):
+    print(member)
+    result = MemberService.insert_member(db, member)
+    print('처리결과 : ', result.rowcount)
+
 
 @member_router.get('/login', response_class=HTMLResponse)
 async def login(req: Request):
