1676f918c694a6f197aa7363ddf510891c8f5f97
pq100 <system32bit@naver.com>
2024-08-22 11:43:40 +0900

    로그인 여부에 따른 myinfo 표시 처리

--- app/routes/member.py
+++ app/routes/member.py
@@ -61,7 +61,16 @@ async def error(req: Request):
 
 @member_router.get('/myinfo', response_class=HTMLResponse)
 async def myinfo(req: Request):
-    return templates.TemplateResponse('member/myinfo.html', {'request': req})
+    try:
+        if 'logined_uid' not in req.session:  # 로그인하지 않았다면
+            return RedirectResponse(url='/member/login', status_code=303)
+
+        return templates.TemplateResponse('member/myinfo.html', {'request': req})
+
+    except Exception as ex:
+        print(f'▷▷▷ myinfo 오류 발생 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
+
 
 @member_router.get('/error', response_class=HTMLResponse)
 async def error(req: Request):
