774aa256050909756c754036fa4141d7253fbd06
pq100 <system32bit@naver.com>
2024-08-27 13:54:57 +0900

    윈도우 함수를 이용해서 1:n 관계의 데이터를 하나씩 리스트로 출력

--- app/service/gallery.py
+++ app/service/gallery.py
@@ -1,12 +1,11 @@
 import os
 from datetime import datetime
 
-
 import attach
 from fastapi import Form
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, distinct, func
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import aliased
+
 
 from app.model.gallery import Gallery, GalAttach
 from app.schema.gallery import NewGallery
@@ -60,9 +59,16 @@ class GalleryService:
 
     @staticmethod
     def select_gallery(cpg, db):
+        # select distinct g.gno, title, userid, g.regdate, views,
+        # first_value(fname) over (partition by g.gno) fname
+        # from gallery g join galattach ga
+        # on g.gno = ga.gno
+        # order by g.gno desc;
         try:
-            stmt = select(Gallery.gno, Gallery.title, Gallery.userid, Gallery.regdate,
-                          Gallery.views, GalAttach.fname)\
+            stmt = select(distinct(Gallery.gno).label('gno'),
+                          Gallery.title, Gallery.userid, Gallery.regdate,
+                          Gallery.views, func.first_value(GalAttach.fname)\
+                          .over(partition_by=Gallery.gno).label('fname'))\
                 .join_from(Gallery, GalAttach)\
                 .order_by(Gallery.gno.desc())
             result = db.execute(stmt)
