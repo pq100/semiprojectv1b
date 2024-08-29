fe3d17b2c6b2d7eb41e4b061c33bf56c2bf7c7e2
pq100 <system32bit@naver.com>
2024-08-29 10:18:31 +0900

    댓글 모델 추가 및 1:N 관계 설정

--- app/model/board.py
+++ app/model/board.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
 
 
-class Board(Base):
+class Reply(Base):
     __tablename__ = 'board'
 
     bno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
@@ -15,3 +15,18 @@ class Board(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
+    replys = relationship('Reply', back_populates='board')
+
+
+
+    class Reply(Base):
+        __tablename__ = 'board'
+
+    rno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    Reply: Mapped[str] = mapped_column(index=True)
+    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True)
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    bno: Mapped[int] = mapped_column(ForeignKey('board.bno', index=True))
+    rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno', index=True))
+    contents: Mapped[str]
+    board = relationship('Board', back_populates='replys')
