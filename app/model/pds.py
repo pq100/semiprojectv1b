bd2f1679917f7c53ed5323519e180eb3f8dfea1c
pq100 <system32bit@naver.com>
2024-09-03 11:02:05 +0900

    sqlite모델을 mariadb로 마이그레이션 수정4

--- app/model/pds.py
+++ app/model/pds.py
@@ -5,29 +5,32 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
 
-
 # back_populates : 양방향 관계설정, 관계의 상호참조
 class Pds(Base):
     __tablename__ = 'pds'
 
     pno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    title: Mapped[str] = mapped_column(String(200), index=True)
+    title: Mapped[str] = mapped_column(String(100), index=True)
     userid: Mapped[str] = mapped_column(String(18), ForeignKey('member.userid'), index=True)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str] = mapped_column(Text)
-    attachs = relationship('PdsAttach', back_populates='pds')    # 하나의 pds는 하나 이상의 attach가 존재  (1:n)
-
+    attachs = relationship('PdsAttach', back_populates='pds')  # 하나의 gallery는 하나 이상의 attach 존재 (1:n)
 
 class PdsAttach(Base):
     __tablename__ = 'pdsattach'
 
     pano: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    pno: Mapped[int] = mapped_column(String(250), ForeignKey('pds.pno'), index=True)
-    fname: Mapped[str] = mapped_column(String(18), nullable=False)
-    fsize: Mapped[int] = mapped_column(default=0, nullable=False)
-    regdate: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
-    pds = relationship('Pds', back_populates='attachs')    # 하나의 attach는 하나의 pds에 속함 (1:1)
+    pno: Mapped[int] = mapped_column(ForeignKey('pds.pno'), index=True)
+    fname: Mapped[str] = mapped_column(String(35), nullable=False)
+    fsize: Mapped[int] = mapped_column(default=0)
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    pds = relationship('Pds', back_populates='attachs')  # 하나의 attach는 하나의 gallery에 속함 (1:1)
+
+
+
+
+
 
 
 
